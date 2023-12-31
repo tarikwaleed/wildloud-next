791a786522eeb4a20e7b9ad94e8ed01e7f2a1f7e
tarikwaleed <tarikwaleed.tech@gmail.com>
2023-12-31 15:50:10 +0200

    :hammer: adding logo component

---
+++ src/app/components/logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const Logo = (props: Props) => {
+    return (
+        <div>
+            <img src="/public/images/logo.png" alt="App Logo" />
+        </div>
+    );
+};
+
+export default Logo;
